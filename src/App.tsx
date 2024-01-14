bccc1c0c889bf053bd5671fc531a8ca8eec89195
rugefx <zackfxg@gmail.com>
2024-01-15 01:20:55 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import AriaTest from "./components/aria-test"
+
+function App() {
+  return (
+    <main>
+      <h1 className="font-bold text-3xl text-blue-500">
+        Hello world!
+      </h1>
+      <AriaTest />
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/aria-test.tsx
@@ -0,0 +1,7 @@
+import { Button } from 'react-aria-components';
+
+export default function AriaTest() {
+  return (
+    <Button className="bg-black text-white font-bold px-5 py-3 rounded-lg pressed:opacity-75">Button</Button>
+  )
+} 
